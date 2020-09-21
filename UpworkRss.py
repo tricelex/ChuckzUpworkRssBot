da8dea24bc146e4b0c5e4197aa9b18050e20c363
Chuckz Okoye <tricelex@gmail.com>
2020-09-21 03:34:51 +0100

    Initial commit

---
+++ UpworkRss.py
@@ -0,0 +1,11 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ParseMode
+from util.filehandler import FileHandler
+
+
+class UpworkRss(object):
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    UpworkRss()
